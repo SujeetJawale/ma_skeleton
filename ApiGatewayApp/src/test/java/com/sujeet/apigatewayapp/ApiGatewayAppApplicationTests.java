0c19f31cd05ef7c52c431c6a19d82d8c47354087
SujeetJawale <jawalesujeet10@gmail.com>
2024-08-15 23:13:53 -0700

    added API-Gateway

---
+++ ApiGatewayApp/src/test/java/com/sujeet/apigatewayapp/ApiGatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujeet.apigatewayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
